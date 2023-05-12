8f5c5a93ea9ac90f52b3cca7be87a13b42a64edb
Ambria Johnson <1223ambria@gmail.com>
2023-05-12 13:32:22 -0400

    updat 1.0

--- server.js
+++ server.js
@@ -4,7 +4,45 @@ const mongodb = require('mongodb').MongoClient;
 const app = express();
 const PORT = 3001;
 
+let db;
+
 const connectionStringURI = 'mongodb://localhost:27017/socialDB';
+
+mongodb.connect(
+    connectionStringURI, {
+        useNewUrlParser: true, useUnifiedTopology: true},
+        (err,client) => {
+            db = client.db();
+            app.listen(port, () => {
+                console.log(`app listening at http://localhost:${port}`);
+            });
+        }
+    
+);
+
+app.use(express.json());
+
+app.post('/create', (req, res) => {
+  // Use db connection to add a document
+  db.collection('').insertOne(
+    { name: req.body.name, breed: req.body.breed },
+    (err, results) => {
+      if (err) throw err;
+      res.json(results);
+    }
+  );
+});
+
+app.get('/read', (req, res) => {
+  // Use db connection to find all documents in collection
+  db.collection('petCollection')
+    .find()
+    .toArray((err, results) => {
+      if (err) throw err;
+      res.send(results);
+    });
+});
+
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
